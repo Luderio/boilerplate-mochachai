ec39b9bfd9288ca5edeb934d63164a7a551e37af
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2021-12-22 12:19:23 +0800

    FUnctional tests Lesson 2

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -26,7 +26,7 @@ suite('Functional Tests', function () {
         .request(server)
         .get('/hello?name=Luderio')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
+          assert.equal(res.status, 200);
           assert.equal(res.text, 'hello Luderio');
           done();
         });
